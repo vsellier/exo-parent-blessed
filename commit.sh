6ff35fc6b6c3a4c7b65cc14f66f5a56e093f3e2c
Vincent Sellier <vsellier@exoplatform.com>
2018-09-11 10:30:04 +0200

    update modules

--- commit.sh
+++ commit.sh
@@ -2,12 +2,14 @@
 
 projects=$(git status -s | grep -v -e "^A" -e "^M" | awk '{print $2}')
 
-MSG=$1
+MSG="$*"
 
 for p in $projects
 do
   pushd $p
   echo $p
+  git add -A
   git commit -m "${MSG}"
+  gitk --all
   popd
 done
